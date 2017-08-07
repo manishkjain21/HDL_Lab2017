fdfb2b85b119e6bc8647e9b686af1d4f44b7b694
Manish Jain <manishkjain21@gmail.com>
2017-08-07 17:17:25 +0200

    updated cpu test bench :)

---
+++ cpu_tb.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ps
+
+module cpu_tb();
+
+wire [11:0]addr_tb;
+wire [15:0]dout;
+wire mem_en, mem_read, mem_write;
+reg clk, reset;
+reg [15:0]din;
+
+cpu cp1(.clk(clk), 
+.reset(reset), 
+.addr_tb(.addr_tb), 
+.mem_en(mem_en), 
+.mem_read(mem_read), 
+.mem_write(mem_write), 
+.dout(dout), 
+.din(din));
+
+initial begin 
+	clk = 0; 
+	reset = 0; 
+	#5 din = 16'hB805;
+   #20 $finish;
+	end  
+
+always begin
+	#5 clk =!clk 
+	end
+
+always begin
+	$monitor("%d, \t%b, \t%b, \t%h, \t%h, \t%h, \t%b, \t%b, \t%b",
+           $time, clk, reset, din, addr_tb, dout, mem_en, mem_read, mem_write);
+	end
+
+endmodule
